3fe503deed03314937ba808504c9c0cb6df92b65
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-08-10 21:59:47 +0530

    Update VariableExample.java

--- VariableExample.java
+++ VariableExample.java
@@ -1,1 +1,9 @@
+// for variables:-  data_type  VariableName = Value;
+public class VariableExample {
+    public static void main(String[] args) {
+        // Write your code here 💖  
+    String name = "Siya";          
+    System.out.println("Hello " + name);
+    }
+}
 
