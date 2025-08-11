594ac7c39fb0da9b5c8447bc3813c241f1dc65d5
ShivaniDixit05 <shivanidixit0525@gmail.com>
2025-08-11 20:33:12 +0530

    Update FavouriteSong.java

--- FavouriteSong.java
+++ FavouriteSong.java
@@ -1,1 +1,24 @@
+/**
+ * Woah? What's this? 🤔 It's a weird looking comment. Another *, eh?
+ * Write a little bit about yourself here. What's your name? Where are you from? What do you do?
+ *  
+ * I'm a comment, and I want to say hi! 👋
+ */
+
+public class FavouriteSong {
+    public static void main(String[] args) {
+        System.out.println("========== 🎵 Music Wrapped 🎵 ==========");
+
+        // Write your code here! 💖
+        System.out.println("Tere bina"); //A.R Rahman
+        System.out.println("Ishq di bajiyaan");//Diljit
+        System.out.println("Salam-e-ishq");//Sonu nigam
+        System.out.println("Ye tune kya kiya");//pritam
+        System.out.println("Channa ve");//Akhil
+
+
+
+        System.out.println("======= 🎵 🎵 🎵 🎵 🎵 🎵 🎵 🎵 ========");
+    }
+}
 
